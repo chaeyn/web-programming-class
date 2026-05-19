ffa4eb4af04f81aef5e55ad231e8d6db76699184
chaeyn <chaulfe@gmail.com>
2026-05-19 12:16:32 +0900

    260519 useRef

--- chapter5/src/components/Body.jsx
+++ chapter5/src/components/Body.jsx
@@ -1,48 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-function Body({ name, location, breed, children }) {
-  const [state, setState] = useState({
-    username: "",
-    gender: "",
-    birth: "",
-    bio: "",
-  });
+function Body() {
+  const [text, setText] = useState("");
+  const textRef = useRef();
 
   const handleOnChange = e => {
-    const { name, value } = e.target;
+    setText(e.target.value);
+  };
 
-    console.log(`현재 수정 대상: ${name}`);
-    console.log(`수정 값: ${value}`);
-
-    setState({
-      ...state,
-      [name]: value,
-    });
+  const handleOnClick = () => {
+    if (text.length <= 5) {
+      textRef.current.focus();
+    } else {
+      alert(text);
+      setText("");
+    }
   };
 
   return (
     <div>
-      <div>
-        <input
-          name="username"
-          value={state.username}
-          onChange={handleOnChange}
-          placeholder="이름"
-        />
-      </div>
-      <div>
-        <select name="gender" value={state.gender} onChange={handleOnChange}>
-          <option value="" />
-          <option value="남성">남성</option>
-          <option value="여성">여성</option>
-        </select>
-      </div>
-      <div>
-        <input name="birth" type="date" value={state.birth} onChange={handleOnChange} />
-      </div>
-      <div>
-        <textarea name="bio" value={state.bio} onChange={handleOnChange} />
-      </div>
+      <input ref={textRef} value={text} onChange={handleOnChange} />
+      <button onClick={handleOnClick}>작성완료</button>
     </div>
   );
 }
